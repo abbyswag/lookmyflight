b671c816a001c77288ed99105e872c395e2415d9
abbyswag <abbyswag25@gmail.com>
2024-02-21 02:17:36 +0530

    new

--- flightdesk/views.py
+++ flightdesk/views.py
@@ -3,11 +3,11 @@ from django.contrib import messages
 from .models import Booking
 
 
-def approve_booking(request, mybooking_id):
-  booking = get_object_or_404(Booking, mybooking_id=mybooking_id)
+def approve_booking(request, booking_id):
+  booking = get_object_or_404(Booking, booking_id=booking_id)
   booking.status = 'allocating'
   booking.save()
   messages.success(request, 'Booking approved.')
   return render(request, 'approved.html', {
-    'mybooking_id': mybooking_id
+    'mybooking_id': booking_id
   })
